08d711836fc794215f35b2de12fe7a3c736acf5d
korzin <Samkorzin@gmail.com>
2017-09-27 00:00:28 +0300

    Delete unnecessary pojos lefted from spring-guides rest-consumer template.

--- src/main/java/com/korzin/springtemplate/Application.java
+++ src/main/java/com/korzin/springtemplate/Application.java
@@ -12,23 +12,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 	}
-	
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-			log.info(quote.toString());
-		};
-	}
+
 }
