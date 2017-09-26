09297c12d442fccf77518a2c65e7ee0fd96ac9c6
korzin <Samkorzin@gmail.com>
2017-09-26 23:54:41 +0300

    Change default packages' names

---
+++ src/main/java/com/korzin/springtemplate/config/ComponentConfig.java
@@ -0,0 +1,12 @@
+package com.korzin.springtemplate.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.korzin.springtemplate")
+public class ComponentConfig {
+
+
+}
--- src/test/java/com/korzin/springtemplate/ApplicationTest.java
+++ src/test/java/com/korzin/springtemplate/ApplicationTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package hello;
+package com.korzin.springtemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
